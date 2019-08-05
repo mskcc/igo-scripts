30af752b2523b5568a7d1a3ba6c265b8f7c81618
DavidStreid <david.streid.nyc@gmail.com>
2019-08-05 14:37:21 -0400

    Adding logger & refactoring

--- src/igo_transfer.py
+++ src/igo_transfer.py
@@ -2,14 +2,22 @@ import os
 import sys
 from shutil import copyfile
 from datetime import datetime
+import logging
+import transfer_cfg as cfg
 
-LOG = False
+def setup_logger(directory,run):
+    """
+    Sets up file program will log to. Kept at [ROOT]/logs/[RUN].log
 
-
-def log(msg):
-    if LOG:
-        print(msg)
-
+    :param (directory) string: Name of directory to place log file
+    :param (run) string: timestamp name of file, e.g. '08052019.141626.log'
+    """    
+    log_dir='%s/logs' % directory
+    log_file='%s/%s.log' % (log_dir,run)
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)       
+    logging.basicConfig(filename=log_file, level=logging.DEBUG)
+    print('logging to %s' % log_file)
 
 def extract_well_index(well):
     """
@@ -19,11 +27,11 @@ def extract_well_index(well):
     :return: int[]
     """
     if len(well) != 3:
-        log('Well should be of format X##, e.g. \'AO1\': %s' % well)
+        logging.error('Well should be of format X##, e.g. \'AO1\': %s' % well)
     if not well[0].isalpha():
-        log('First character of well should be a letter: %s' % well)
+        logging.error('First character of well should be a letter: %s' % well)
     if not well[1:].isdigit():
-        log('Last characters of well should be digits: %s' % well)
+        logging.error('Last characters of well should be digits: %s' % well)
 
     well_char = well[0].lower()  # a: 97, z: 122
     well_pos = int(well[1:])
@@ -39,20 +47,20 @@ def get_pos_info(file):
     is why there's logic to take the difference when calculating the column's index.
         e.g. 'P05c2_002_001.tif' -> [46, 58, 'c2']
 
-    :param (str) file: Name of file, e.g. "A01c1_002_003.tif"
+    :param (str) file: Name of file, e.g. 'A01c1_002_003.tif'
     :return str[]: array of row, column, and run
     """
     file_extension = '.tif'
-    file = file.strip(file_extension)
-    attr = file.split('_')
+    striped_file = file.rstrip(file_extension)
+    attr = striped_file.split('_')
     if len(attr) != 3:
         raise ValueError('Created file should have format [POS][RUN]_[COL]_[ROW]: %s' % file)
 
-    run = attr[0][-2:]  # "A01c1_002_003" -> "c1"
-    well = attr[0][-5:-2]  # "A01c1_002_003" -> "A01"
-    [col_idx, row_idx] = extract_well_index(well)  # "A01c1_002_003" -> [0,0]
-    rel_col = int(attr[1])  # "A01c1_002_003" -> 2
-    rel_row = int(attr[2])  # "A01c1_002_003" -> 3
+    run = attr[0][-2:]                              # 'A01c1_002_003' -> 'c1'
+    well = attr[0][-5:-2]                           # 'A01c1_002_003' -> 'A01'
+    [col_idx, row_idx] = extract_well_index(well)   # 'A01c1_002_003' -> [0,0]
+    rel_col = int(attr[1])                          # 'A01c1_002_003' -> 2
+    rel_row = int(attr[2])                          # 'A01c1_002_003' -> 3
 
     # Wells are 3x3
     row = (row_idx * 3) + rel_row
@@ -84,7 +92,7 @@ def copy_file_to_igo_dir(nikon_file, igo_dir, row, col, run):
     :param (str) igo_dir: Name of igo directory
     :param (int) row: igo row
     :param (int) col: igo column
-    :param (str) run: run, e.g. "c1" or "c2"
+    :param (str) run: run, e.g. 'c1' or 'c2'
     """
     row = 'R%02d' % row
     col = 'C%02d' % col
@@ -97,7 +105,6 @@ def copy_file_to_igo_dir(nikon_file, igo_dir, row, col, run):
 
     file_path = '%s/%s' % (dir_path, name)
     copy_file = copyfile(nikon_file, file_path)
-    log('Copied %s to %s' % (nikon_file, copy_file))
 
 
 def transfer_to_igo_dir(igo_dir='.', nikon_dir='.'):
@@ -108,30 +115,30 @@ def transfer_to_igo_dir(igo_dir='.', nikon_dir='.'):
     :param (str) nikon_dir: Directory nikon images will be created in
     :return:
     """
-    print('Starting transfer from %s to %s' % (nikon_dir, igo_dir))
+    logging.info('Starting transfer from %s to %s' % (nikon_dir, igo_dir))
     files = os.listdir(nikon_dir)
     ct = 0
     for file in files:
         try:
             [row, column, run] = get_pos_info(file)
         except ValueError as err:
-            print('Did not copy file %s - %s' % (file, err))
+            logging.error('Did not copy file %s - %s' % (file, err))
             continue
         try:
             nikon_file = '%s/%s' % (nikon_dir, file)
             copy_file_to_igo_dir(nikon_file, igo_dir, row, column, run)
             ct += 1
         except OSError as err:
-            print('Error copying %s to %s' % (nikon_dir, dir))
-    print('Transferred %d files to %s' % (ct, igo_dir))
+            logging.error('Error copying %s to %s' % (nikon_dir, dir))
+    logging.info('Transferred %d files to %s' % (ct, igo_dir))
     return
 
 
 if __name__ == '__main__':
     """
     If called w/o arguments, src & dest directory locations are relative to path script is executed at. Clone repo
-    at level of "Transfer" (Location of nikon images). This will create renamed directories named by their
-    timestamp in a "renamed" directory
+    at level of 'Transfer' (Location of nikon images). This will create renamed directories named by their
+    timestamp in a 'renamed' directory
      
     > python igo-scripts/src/igo_transfer.py
     
@@ -140,20 +147,25 @@ if __name__ == '__main__':
     
     > python igo-scripts/src/igo_transfer.py {dest_dir} {src_dir}
     """
-    target_dir = '%s/renamed/%s' % (os.getcwd(), datetime.now().strftime("%m%d%Y_%H:%M:%S"))
-    src_dir = '%s/Transfer' % os.getcwd()
-    if len(sys.argv) == 3:
-        target_dir = sys.argv[1]
-        src_dir = sys.argv[2]
-    elif len(sys.argv) != 1:
-        print('Usage: "python3 igo_transfer.py" or "python3 igo_transfer.py {dest_dir} {src_dir}"')
-        sys.exit()
-
-    if not os.path.isdir(src_dir):
-        print('Could not find source directory - %s. Use "python3 igo_transfer.py {dest_dir} {src_dir}"' % src_dir)
-        sys.exit()
-    if not os.path.isdir(target_dir):
-        print("Creating %s" % target_dir)
-        os.makedirs(target_dir)
-
-    transfer_to_igo_dir(target_dir, src_dir)
+    try:
+        root = cfg.root
+        run = datetime.now().strftime('%m%d%Y.%H%M%S')
+        target_dir = '%s\/renamed\/%s' % (root, run)
+        src_dir = '%s\/Transfer' % root
+        
+        setup_logger(root,run) 
+        if len(sys.argv) == 3:
+            target_dir = sys.argv[1]
+            src_dir = sys.argv[2]
+        elif len(sys.argv) != 1:
+            print('Usage: \'python3 igo_transfer.py\' or \'python3 igo_transfer.py {dest_dir} {src_dir}\'')
+            sys.exit()
+        if not os.path.isdir(src_dir):
+            logging.error('Could not find source directory - %s. Use \'python3 igo_transfer.py {dest_dir} {src_dir}\'' % src_dir)
+            sys.exit()
+        if not os.path.isdir(target_dir):
+            logging.info('Creating %s' % target_dir)
+            os.makedirs(target_dir)          
+        transfer_to_igo_dir(target_dir, src_dir)
+    except Exception as e:
+        logging.error(e)
