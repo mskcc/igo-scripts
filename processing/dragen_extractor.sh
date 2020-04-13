a6236f0f2c8707ddb2ffbfe2102485362551d8c2
DavidStreid <david.streid.nyc@gmail.com>
2020-04-13 19:03:33 -0400

    filtering output

--- processing/dragen_extractor.sh
+++ processing/dragen_extractor.sh
@@ -1,21 +1,30 @@
 # Run this in the output-directory of the dragen run
 
 DRAGEN_MAPPING_FILE="mapping_metrics.csv"
-DRAGEN_OVERALL_COV_FILE="overall_mean_cov.csv"
-DRAGEN_COV_METRICS_FILE="coverage_metrics.csv"
+DRAGEN_OVERALL_COV_FILE="overall_mean_cov*.csv"
+DRAGEN_COV_METRICS_FILE="coverage_metrics*.csv"
 
+# "MAPPING/ALIGNING SUMMARY,,Q30 bases," 
+# "Number of unique reads (excl. duplicate marked reads),"
 DRAGEN_MAPPING_FIELDS=(
+   "Properly paired reads"
+   "Not properly paired reads,"
+   "Unmapped reads,"
+   "Number of duplicate marked reads,"
    "Total input reads,"
    "Total bases,"
-   "Number of unique reads (excl. duplicate marked reads),"
-   "Number of unique reads (excl. duplicate marked reads),"
-   "Total bases,"
-   "MAPPING/ALIGNING SUMMARY,,Q30 bases,"
 )
+# "Average alignment coverage over"
 DRAGEN_OVERALL_COV_FIELDS=(
-   "Average alignment coverage over"
+   "Average alignment coverage over target_bed"
 )
+
+# "Aligned reads"
+# "Aligned bases"
 DRAGEN_COV_METRICS_FIELDS=(
+   "Aligned reads in target region"
+   "Aligned bases in target region"
+   "Average alignment coverage over target region"
    "1x: inf"
    "10x: inf"
    "20x: inf"
@@ -23,17 +32,20 @@ DRAGEN_COV_METRICS_FIELDS=(
    "100x: inf"
 )
 
+# WGS
 all_file_types=($DRAGEN_MAPPING_FILE $DRAGEN_OVERALL_COV_FILE $DRAGEN_COV_METRICS_FILE)
 for file_type in ${all_file_types[@]}
 do
-   target_files=$(find . -type f -name "*${file_type}")
+   # If not WES, remove 'grep -v "wgs_coverage_metrics"'
+   target_files=$(find . -type f -name "*${file_type}" | grep -v "wgs_coverage_metrics")
    for file in ${target_files[@]}
    do
       for field in "${DRAGEN_MAPPING_FIELDS[@]}"; do
-         grep -i "$field" $file
+         grep -i "$field" $file | grep -v "PER RG"
       done
+      # echo $file # tumor/normal are kept in seperate files
       for field in "${DRAGEN_OVERALL_COV_FIELDS[@]}"; do
-         grep -i "$field" $file
+         grep "$field" $file
       done
       for field in "${DRAGEN_COV_METRICS_FIELDS[@]}"; do
          grep "$field" $file
