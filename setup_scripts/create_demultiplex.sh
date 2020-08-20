dafed73387d42f1921cb9535238c64013fca78c9
DavidStreid <david.streid.nyc@gmail.com>
2020-08-20 08:50:48 -0400

    Updating options to write project dirs in demux

--- setup_scripts/create_demultiplex.sh
+++ setup_scripts/create_demultiplex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z $1 || -z $2 || -z $3 ]]; then
-  printf "usage: ./create_demultiplex.sh in out\n"
+  printf "usage: ./create_demultiplex.sh in out sample_sheet\n"
   exit 1
 fi
 
@@ -9,14 +9,15 @@ INPUT_DIR=$1
 OUTPUT_DIR=$2
 SAMPLE_SHEET=$3
 
-CMD="/opt/edico/bin/dragen --bcl-conversion-only true \\
-  --bcl-input-directory ${INPUT_DIR} \\
-  --output-directory ${OUTPUT_DIR} \\
+CMD="/opt/edico/bin/dragen --bcl-conversion-only true \
+  --bcl-input-directory ${INPUT_DIR} \
+  --output-directory ${OUTPUT_DIR} \
+  --bcl-sampleproject-subdirectories true \
   --sample-sheet ${SAMPLE_SHEET}"
 
-echo "Running command: ${CMD}"
+echo "Writing command: ${CMD}"
+JOB_NAME=$(basename $3 | cut -d'.' -f1)
 
-JOB_NAME=$(basename $1)
-bsub -J $JOB_NAME -o ${JOB_NAME}.out -e ${JOB_NAME}.err -n48 -q dragen "${CMD}"
+echo "bsub -J $JOB_NAME -o ${JOB_NAME}.out -e ${JOB_NAME}.err -n48 -q dragen \"${CMD}\"" >  ${JOB_NAME}.sh
 
 
